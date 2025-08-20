6c3a3d84e3f628a2704774839be2938124b7ec63
Remy Chauveau <rem.chauveau@gmail.com>
2025-08-20 19:37:51 +0200

    🚧 Try to escape the webview !

--- apps/tgapp/src/app/page.tsx
+++ apps/tgapp/src/app/page.tsx
@@ -7,7 +7,12 @@ export default function Home() {
   const isWebview = useIsWebView();
   return (
     <>
-      <div className="flex flex-col gap-4 p-4 bg-red-500 rounded-lg">
+      <div
+        className="flex flex-col gap-4 p-4 bg-red-500 rounded-lg"
+        onClick={() => {
+          window.open("http://metal-quest-tg-app.vercel.app", "_blank");
+        }}
+      >
         <p>Hey open this in pure browser</p>
       </div>
       <OnboardingStepper></OnboardingStepper>
