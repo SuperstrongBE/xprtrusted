6dacfce3a1713101fedc1b1f2d5ba1e27d65292a
Remy Chauveau <rem.chauveau@gmail.com>
2025-08-20 20:26:39 +0200

    ⚡️ Add conditional rendering for webview.

--- apps/tgapp/src/app/page.tsx
+++ apps/tgapp/src/app/page.tsx
@@ -1,23 +1,24 @@
 "use client";
-
 import {OnboardingStepper} from "@/components/03_organisms/onboarding-stepper/onboarding-stepper";
 import {useIsWebView} from "@/hooks/useWebview";
 
 export default function Home() {
   const {isWebView, context} = useIsWebView();
+
   return (
     <>
-      <div
-        className="flex flex-col gap-4 p-4 bg-red-500 rounded-lg"
-        onClick={() => {
-          window.open("http://metal-quest-tg-app.vercel.app", "_blank");
-        }}
-      >
-        <p>Hey open this in pure browser</p>
-      </div>
-      <OnboardingStepper></OnboardingStepper>
-      <p>{isWebView && context === "browser" ? "browser" : "webview"}</p>
-      <p>{context}</p>
+      {isWebView && context === "browser" ? (
+        <OnboardingStepper></OnboardingStepper>
+      ) : (
+        <div
+          className="flex flex-col gap-4 p-4 bg-red-500 rounded-lg"
+          onClick={() => {
+            window.open("http://metal-quest-tg-app.vercel.app", "_blank");
+          }}
+        >
+          <p>Start the trusting process on your browser</p>
+        </div>
+      )}
     </>
     // <ButtonTest></ButtonTest>
   );
