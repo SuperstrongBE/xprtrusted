99b3354237eeb2c92ca477d04a38c4b40e39647c
Remy Chauveau <rem.chauveau@gmail.com>
2025-08-27 01:37:43 +0200

    Fix env var

--- apps/tgapp/src/lib/auth.ts
+++ apps/tgapp/src/lib/auth.ts
@@ -2,7 +2,12 @@ import NextAuth from "next-auth";
 import Twitter from "next-auth/providers/twitter";
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
-  providers: [Twitter],
+  providers: [
+    Twitter({
+      clientId: process.env.AUTH_TWITTER_ID!,
+      clientSecret: process.env.AUTH_TWITTER_SECRET!,
+    }),
+  ],
   callbacks: {
     async session({session, token}) {
       if (token?.sub) {
